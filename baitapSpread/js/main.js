bfafed0d5734005bcb9ccc8b74a4bada0c284914
ducthang142 <anubin142@gmail.com>
2023-01-06 19:39:53 +0700

    complete

---
+++ baitapSpread/js/main.js
@@ -0,0 +1,7 @@
+let heading = document.querySelector('.heading').innerHTML.split('');
+let arrs = [...heading];
+let content = ''
+for (let key of arrs) {
+    content += `<span>${key}</span>`
+}
+document.querySelector('.heading').innerHTML = content;
